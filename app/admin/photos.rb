f04214f2494611bcbd6b229f75e7705d6d5240c7
Raghu Betina <raghu@firstdraft.com>
2020-04-13 15:35:04 +0000

    Whitelist columns in Photo for ActiveAdmin

--- app/admin/photos.rb
+++ app/admin/photos.rb
@@ -1,4 +1,6 @@
 ActiveAdmin.register Photo do
+
+ permit_params :post_id, :image
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
